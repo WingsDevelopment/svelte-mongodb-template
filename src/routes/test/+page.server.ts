df7891f10275dead1371686a64d0f9392d005ca6
Srdjan Rakic <srdjanr.1609@gmail.com>
2022-12-10 13:53:58 +0100

    fixed ts

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,11 +1,11 @@
 import db from '$lib/db';
 import type { PageServerLoad } from './$types';
 
-export const load = (<PageServerLoad>(async ({ params }) => {
+export const load = (async ({ params }) => {
 	const cars = await db.collection('cars').find().toArray();
 	console.log(cars);
 	return {
 		status: 200,
 		cars: cars.map((car) => ({ name: car.name }))
 	};
-})) satisfies PageServerLoad;
+}) satisfies PageServerLoad;
